6bfcc65c229f580dccf9ae5fd392a5d5de5290dd
Alex Sunderland <alex.sunderland@gmail.com>
2014-05-12 16:17:18 +0100

    Add database support

--- notebook.rb
+++ notebook.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
-
 require 'socket'
+require 'sqlite3'
 
 CRLF = "\r\n"
 
 server = TCPServer.new(9292)
+database = SQLite3::Database.new('notebook.sqlite3')
 
 loop do
   socket = server.accept
@@ -20,7 +21,23 @@ loop do
   socket.write 'HTTP/1.1 200 OK' + CRLF
   socket.write 'Content-Type: text/html' + CRLF
   socket.write CRLF
-  socket.write "request_method: #{request_method}, request_path: #{request_path}, http_version: #{http_version}" + CRLF
+
+  response_body = ''
+
+  case [request_method, request_path]
+  when ['GET', '/show-notes']
+    response_body << '<ul>'
+    # show ALL the notes
+    database.execute('SELECT * FROM notes') do |(content)|
+      response_body << "<li>#{content}</li>"
+    end
+
+    response_body << '</ul>'
+  else
+    response_body << ("request_method: #{request_method}, request_path: #{request_path}, http_version: #{http_version}" + CRLF)
+  end
+
+  socket.write response_body
   socket.close
 end
 
