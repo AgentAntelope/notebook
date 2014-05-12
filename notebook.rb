035e7670287a15c148ab57da59d2e724d3dea7cc
Alex Sunderland <alex.sunderland@gmail.com>
2014-05-12 16:01:45 +0100

    Conform to specification regarding CLRF

--- notebook.rb
+++ notebook.rb
@@ -2,6 +2,8 @@
 
 require 'socket'
 
+CRLF = "\r\n"
+
 server = TCPServer.new(9292)
 
 loop do
@@ -15,7 +17,10 @@ loop do
   request_line, *header_lines = lines
   request_method, request_path, http_version =  request_line.split(' ')
 
-  socket.puts "request_method: #{request_method}, request_path: #{request_path}, http_version: #{http_version}"
+  socket.write 'HTTP/1.1 200 OK' + CRLF
+  socket.write 'Content-Type: text/html' + CRLF
+  socket.write CRLF
+  socket.write "request_method: #{request_method}, request_path: #{request_path}, http_version: #{http_version}" + CRLF
   socket.close
 end
 
